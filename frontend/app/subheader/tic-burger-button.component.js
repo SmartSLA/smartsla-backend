afa6f82cf06d7571acdd7862117c2a53cbeec912
Renaud BOYER <rboyer@linagora.com>
2016-10-27 15:04:42 +0200

    #1 Unify subheader buttons

---
+++ frontend/app/subheader/tic-burger-button.component.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('linagora.esn.ticketing')
+
+  .component('ticBurgerButton', {
+    templateUrl: '/linagora.esn.ticketing/app/subheader/tic-burger-button.html'
+  });
