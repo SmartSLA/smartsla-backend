6100bad3179e340cb944bbb869a5e1856b5f5565
Abdulkader BENCHI <abenchi@linagora.com>
2016-12-01 15:47:22 +0100

    54 refactor ticNotificationFactory

--- frontend/app/notification/tic-notification-factory.js
+++ frontend/app/notification/tic-notification-factory.js
@@ -3,41 +3,21 @@
 angular.module('linagora.esn.ticketing')
   .factory('ticNotificationFactory', function($translate, notificationFactory) {
 
-    function weakSuccess(title, text) {
-      $translate([title, text]).then(function(translations) {
-        return notificationFactory.weakSuccess(translations[title], translations[text]);
-      });
-    }
-
-    function weakInfo(title, text) {
-      $translate([title, text]).then(function(translations) {
-        notificationFactory.weakInfo(translations[title], translations[text]);
-      });
-    }
-
-    function weakError(title, text) {
-      $translate([title, text]).then(function(translations) {
-        notificationFactory.weakError(translations[title], translations[text]);
-      });
-    }
-
-    function strongInfo(title, text) {
-      $translate([title, text]).then(function(translations) {
-        notificationFactory.strongInfo(translations[title], translations[text]);
-      });
-    }
-
-    function strongError(title, text) {
-      $translate([title, text]).then(function(translations) {
-        notificationFactory.strongError(translations[title], translations[text]);
-      });
-    }
-
     return {
-      weakInfo: weakInfo,
-      weakError: weakError,
-      weakSuccess: weakSuccess,
-      strongInfo: strongInfo,
-      strongError: strongError
+      weakInfo: translate('weakInfo'),
+      weakError: translate('weakError'),
+      weakSuccess: translate('weakSuccess'),
+      strongInfo: translate('strongInfo'),
+      strongError: translate('strongError')
     };
+
+    /////////////
+
+    function translate(methodName) {
+      return function(title, text) {
+        $translate([title, text]).then(function(translations) {
+          return notificationFactory[methodName](translations[title], translations[text]);
+        });
+      };
+    }
   });
