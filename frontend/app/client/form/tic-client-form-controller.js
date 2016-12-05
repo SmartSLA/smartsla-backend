e6ecbfd931741b86567a29c560f658e8e55858f2
Abdulkader BENCHI <abenchi@linagora.com>
2016-12-05 10:40:35 +0100

    41 use component  instead of timeout

--- frontend/app/client/form/tic-client-form-controller.js
+++ frontend/app/client/form/tic-client-form-controller.js
@@ -4,15 +4,15 @@
   angular.module('linagora.esn.ticketing')
     .controller('ticClientFormController', ticClientFormController);
 
-  function ticClientFormController($scope, ticClientLogoService, $timeout) {
+  function ticClientFormController($scope, ticClientLogoService) {
     var self = this;
 
     $scope.client = self.client;
     self.formName = self.formName || 'form';
     self.getClientLogo = ticClientLogoService.getClientLogo;
 
-    $timeout(function() {
+    self.$postLink = function() {
       self.form = $scope[self.formName];
-    }, 0);
+    };
  }
 })();
