5a3249fdbf32680e6acf6950bfad96e9ce5937df
Tuan CONG LE <tlcong@linagora.com>
2017-11-20 17:12:09 +0700

    #111 Define model for Sofware

---
+++ backend/lib/db/software.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = dependencies => {
+  const mongoose = dependencies('db').mongo.mongoose;
+
+  const SoftwareSchema = new mongoose.Schema({
+    active: { type: Boolean, default: true },
+    name: { type: String, required: true, unique: true },
+    category: { type: String, required: true },
+    versions: [{ type: String, unique: true }],
+    timestamps: {
+      creation: { type: Date, default: Date.now }
+    },
+    schemaVersion: { type: Number, default: 1 }
+  });
+
+  const SoftwareModel = mongoose.model('Software', SoftwareSchema);
+
+  SoftwareSchema.pre('save', function(next) {
+    const self = this;
+
+    // Get the document by name insensitive lowercase and uppercase
+    SoftwareModel.findOne({ name: new RegExp(`^${self.name}$`, 'i') }, (err, software) => {
+      if (err) {
+        return next(err);
+      }
+
+      if (!self.isNew && software._id.toString() !== self._id.toString()) {
+        return next(new Error('name is taken'));
+      }
+
+      if (self.isNew && software) {
+        return next(new Error('name is taken'));
+      }
+
+      next();
+    });
+  });
+
+  return mongoose.model('Software', SoftwareSchema);
+};
---
+++ test/unit-storage/lib/db/software.js
@@ -0,0 +1,72 @@
+'use strict';
+
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('The sofware model', function() {
+  let Software, mongoose;
+
+  beforeEach(function(done) {
+    mongoose = this.moduleHelpers.dependencies('db').mongo.mongoose;
+
+    require(this.testEnv.backendPath + '/lib/db/software')(this.moduleHelpers.dependencies);
+    Software = mongoose.model('Software');
+
+    this.connectMongoose(mongoose, done);
+  });
+
+  afterEach(function(done) {
+    delete mongoose.connection.models.Software;
+    this.helpers.mongo.dropDatabase(err => {
+      if (err) return done(err);
+      this.testEnv.core.db.mongo.mongoose.connection.close(done);
+    });
+  });
+
+  function saveSoftware(sofwareJson, callback) {
+    const sofware = new Software(sofwareJson);
+
+    return sofware.save(callback);
+  }
+
+  describe('The name field', function() {
+    it('should not store the sofware which has name is taken', function(done) {
+      saveSoftware({
+        name: 'foo',
+        category: 'bar',
+        versions: ['1']
+      }, err => {
+        expect(err).to.not.exist;
+
+        const software = {
+          name: 'Foo',
+          category: 'bar',
+          versions: ['1']
+        };
+
+        saveSoftware(software, err => {
+          expect(err).to.exist;
+          expect(err.message).to.equal('name is taken');
+          done();
+        });
+      });
+    });
+
+    it('should store the sofware which has name is taken by itself', function(done) {
+      saveSoftware({
+        name: 'foo',
+        category: 'bar',
+        versions: ['1']
+      }, (err, createdSofware) => {
+        expect(err).to.not.exist;
+        const software = Object.assign(createdSofware, { category: 'bazzz' });
+
+        saveSoftware(software, (err, storedSoftware) => {
+          expect(err).to.not.exist;
+          expect(storedSoftware.category).to.equal(software.category);
+          done();
+        });
+      });
+    });
+  });
+});
