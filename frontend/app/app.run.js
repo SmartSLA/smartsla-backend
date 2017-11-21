737550273ec1fc98870e5363b54d5c24eeeeb18c
Tuan CONG LE <tlcong@linagora.com>
2017-11-21 16:17:32 +0700

    #133 Build and register the search provider for software

--- frontend/app/app.run.js
+++ frontend/app/app.run.js
@@ -6,14 +6,17 @@
   .run(function(
     dynamicDirectiveService,
     TicketingSearchService,
-    TicketingOrganizationService
+    TicketingOrganizationService,
+    TicketingSoftwareService
   ) {
     var group = new dynamicDirectiveService.DynamicDirective(true, 'ticketing-application-menu', { priority: -10 });
     var organiztionSearchProvider = TicketingOrganizationService.getOrganizationSearchProvider();
     var entitySearchProvider = TicketingOrganizationService.getEntitySearchProvider();
+    var softwareSearchProvider = TicketingSoftwareService.getSearchProvider();
 
     TicketingSearchService.addProvider(organiztionSearchProvider);
     TicketingSearchService.addProvider(entitySearchProvider);
+    TicketingSearchService.addProvider(softwareSearchProvider);
     dynamicDirectiveService.addInjection('esn-application-menu', group);
   });
 })(angular);
---
+++ frontend/app/software/ticketing-software.service.js
@@ -0,0 +1,35 @@
+(function(angular) {
+  angular.module('linagora.esn.ticketing')
+    .factory('TicketingSoftwareService', TicketingSoftwareService);
+
+  function TicketingSoftwareService($log, ticketingSoftwareClient) {
+    return {
+      getSearchProvider: getSearchProvider
+    };
+
+    function getSearchProvider() {
+      return {
+        objectType: 'software',
+        templateUrl: '/ticketing/app/software/search-template/ticketing-software-search-template.html',
+        getDisplayName: function(software) {
+          return software.name;
+        },
+        search: function(query, limit) {
+          var searchQuery = {
+            search: query,
+            limit: limit
+          };
+
+          return ticketingSoftwareClient.list(searchQuery)
+            .then(function(response) {
+              return response.data;
+            }, function(err) {
+              $log.error('Error while searching software:', err);
+
+              return $q.when([]);
+            });
+        }
+      };
+    }
+  }
+})(angular);
