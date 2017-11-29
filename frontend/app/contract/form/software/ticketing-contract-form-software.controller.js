4ab18c8e6a16886b4523dcdcc9e100a8ef740c85
Tuan CONG LE <tlcong@linagora.com>
2017-11-29 17:31:27 +0700

    #135 build add software form for contract

---
+++ frontend/app/contract/form/software/ticketing-contract-form-software.controller.js
@@ -0,0 +1,61 @@
+(function(angular) {
+  'use strict';
+
+  angular.module('linagora.esn.ticketing')
+    .controller('TicketingContractFormSoftwareController', TicketingContractFormSoftwareController);
+
+  function TicketingContractFormSoftwareController(
+    $scope,
+    TicketingContractService,
+    _
+  ) {
+    var self = this;
+
+    self.$onInit = $onInit;
+
+    function $onInit() {
+      self.onAddBtnClick = onAddBtnClick;
+      self.softwareIdsExist = _getSoftwareIdsExist(self.contract);
+      self.softwareAvailableTypes = _getSoftwareAvailableTypes();
+
+      $scope.newSoftware = self.softwareTemplate || [];
+      self.software = {};
+      $scope.$watch('newSoftware', function() {
+        self.softwareTemplate = $scope.newSoftware.length ? $scope.newSoftware[0] : {};
+        self.software = {
+          template: self.softwareTemplate,
+          versions: []
+        };
+      }, true);
+    }
+
+    function onAddBtnClick(form) {
+      return TicketingContractService.addSoftware(self.contract._id, self.software)
+        .then(function() {
+          form.$setUntouched(); // reset form to untouched state
+          self.exceptionSoftwareIds = _getSoftwareIdsExist(self.contract);
+          $scope.newSoftware = [];
+          self.software = {};
+        });
+    }
+
+    function _getSoftwareIdsExist(contract) {
+      return _.map(contract.software, function(item) {
+        return item.template._id;
+      });
+    }
+
+    function _getSoftwareAvailableTypes() {
+      var availableTypes = _.map(self.contract.demands, function(demand) {
+        return demand.softwareType;
+      });
+
+      availableTypes.filter(function(value, index) {
+        return availableTypes.indexOf(value) === index;
+      });
+      availableTypes.sort();
+
+      return availableTypes;
+    }
+  }
+})(angular);
