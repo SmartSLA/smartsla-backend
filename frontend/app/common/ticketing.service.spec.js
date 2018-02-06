13962acaa4f4bfe246451a31cdc59182f75b2b5e
Khac Chien LANG <kclang@linagora.com>
2018-02-06 08:54:00 +0700

    #174 modify depopulate function to depopulate attribute of object as array

--- frontend/app/common/ticketing.service.spec.js
+++ frontend/app/common/ticketing.service.spec.js
@@ -16,20 +16,20 @@ describe('The TicketingService', function() {
   });
 
   describe('The depopulate function', function() {
-    it('should depopulate fields which is provided by keys', function() {
+    it('should depopulate given attributes of object', function() {
       var object = {
         a: { _id: 'a' },
         b: 'b',
-        c: { _id: 'c' }
+        c: [{ _id: 'c1' }, { _id: 'c2' }]
       };
-      var keys = ['a', 'c'];
+      var keys = ['a', 'c', 'd'];
 
       TicketingService.depopulate(object, keys);
 
       expect(object).to.deep.equal({
         a: 'a',
         b: 'b',
-        c: 'c'
+        c: ['c1', 'c2']
       });
     });
   });
