8a0714ea479a2c810dd02a18e2d9c59481212883
Khac Chien LANG <kclang@linagora.com>
2018-02-06 09:00:23 +0700

    #174 correct build activity when update software of ticket

--- backend/webserver/api/ticket/controller.js
+++ backend/webserver/api/ticket/controller.js
@@ -258,13 +258,13 @@ module.exports = function(dependencies, lib) {
     }
 
     if (!ticket.software || String(software.template) !== String(ticket.software.template._id)) {
-      const softwareTemplate = _.find(ticket.contract.software, item => item.template._id === software.template);
+      const softwareTemplate = ticket.contract.software.find(item => String(item.template._id) === software.template);
 
       return {
         key: 'software',
         displayName: 'software',
         from: ticket.software ? `${ticket.software.template.name} ${ticket.software.version} - (${ticket.software.criticality})` : '',
-        to: `${softwareTemplate.name} ${software.version} - (${software.type})`
+        to: `${softwareTemplate.template.name} ${software.version} - (${software.criticality})`
       };
     }
 
