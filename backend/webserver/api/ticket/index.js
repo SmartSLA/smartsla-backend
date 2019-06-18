47828448396a98af22b5fee9e58173b404f01e9e
kferjani <kferjani@linagora.com>
2019-06-18 13:55:39 +0100

    added ticket module

--- backend/webserver/api/ticket/index.js
+++ backend/webserver/api/ticket/index.js
@@ -2,58 +2,19 @@
 
 module.exports = (dependencies, lib, router) => {
   const authorizationMW = dependencies('authorizationMW');
-  const { checkIdInParams } = dependencies('helperMw');
   const controller = require('./controller')(dependencies, lib);
   const { loadUserRole } = require('../helpers')(dependencies, lib);
   const {
-    load,
-    loadContract,
     canCreateTicket,
     canListTicket,
     canReadTicket,
-    canUpdateTicket,
-    validateTicketCreation,
-    validateTicketUpdate
+    canUpdateTicket
   } = require('./middleware')(dependencies, lib);
-  const TICKET_POPULATIONS = [
-    {
-      path: 'contract',
-      select: 'title organization demands software defaultSupportManager',
-      populate: [
-        {
-          path: 'organization',
-          select: 'shortName'
-        },
-        {
-          path: 'software.template',
-          select: 'name'
-        }
-      ]
-    },
-    {
-      path: 'requester',
-      select: 'firstname lastname'
-    },
-    {
-      path: 'supportTechnicians',
-      select: 'firstname lastname'
-    },
-    {
-      path: 'supportManager',
-      select: 'firstname lastname'
-    },
-    {
-      path: 'software.template',
-      select: 'name'
-    }
-  ];
 
   router.post('/tickets',
     authorizationMW.requiresAPILogin,
     loadUserRole,
     canCreateTicket,
-    loadContract,
-    validateTicketCreation,
     controller.create
   );
 
@@ -67,8 +28,6 @@ module.exports = (dependencies, lib, router) => {
   router.get('/tickets/:id',
     authorizationMW.requiresAPILogin,
     loadUserRole,
-    checkIdInParams('id', 'Ticket'),
-    load('id', { populations: TICKET_POPULATIONS }),
     canReadTicket,
     controller.get
   );
@@ -76,18 +35,7 @@ module.exports = (dependencies, lib, router) => {
   router.post('/tickets/:id',
     authorizationMW.requiresAPILogin,
     loadUserRole,
-    load('id', { populations: TICKET_POPULATIONS }),
     canUpdateTicket,
-    checkIdInParams('id', 'Ticket'),
-    validateTicketUpdate,
     controller.update
-  );
-
-  router.get('/tickets/:id/activities',
-    authorizationMW.requiresAPILogin,
-    loadUserRole,
-    load('id', { populations: TICKET_POPULATIONS }),
-    canReadTicket,
-    controller.getActivities
   );
 };
