c2bfa9a2f90cbf63f0b4e06a4f759cd04ace12d0
kferjani <kferjani@linagora.com>
2019-06-18 16:36:08 +0100

    access control

--- backend/webserver/api/ticket/index.js
+++ backend/webserver/api/ticket/index.js
@@ -3,34 +3,24 @@
 module.exports = (dependencies, lib, router) => {
   const authorizationMW = dependencies('authorizationMW');
   const controller = require('./controller')(dependencies, lib);
-  const {
-    canCreateTicket,
-    canListTicket,
-    canReadTicket,
-    canUpdateTicket
-  } = require('./middleware')(dependencies, lib);
 
   router.post('/tickets',
     authorizationMW.requiresAPILogin,
-    canCreateTicket,
     controller.create
   );
 
   router.get('/tickets',
     authorizationMW.requiresAPILogin,
-    canListTicket,
     controller.list
   );
 
   router.get('/tickets/:id',
     authorizationMW.requiresAPILogin,
-    canReadTicket,
     controller.get
   );
 
   router.post('/tickets/:id',
     authorizationMW.requiresAPILogin,
-    canUpdateTicket,
     controller.update
   );
 };
