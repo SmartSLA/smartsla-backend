fca58e15f0f702ece27a8c70ab44878b337b307f
kferjani <kferjani@linagora.com>
2019-06-18 16:19:09 +0100

    removed role load

--- backend/webserver/api/ticket/index.js
+++ backend/webserver/api/ticket/index.js
@@ -3,7 +3,6 @@
 module.exports = (dependencies, lib, router) => {
   const authorizationMW = dependencies('authorizationMW');
   const controller = require('./controller')(dependencies, lib);
-  const { loadUserRole } = require('../helpers')(dependencies, lib);
   const {
     canCreateTicket,
     canListTicket,
@@ -13,28 +12,24 @@ module.exports = (dependencies, lib, router) => {
 
   router.post('/tickets',
     authorizationMW.requiresAPILogin,
-    loadUserRole,
     canCreateTicket,
     controller.create
   );
 
   router.get('/tickets',
     authorizationMW.requiresAPILogin,
-    loadUserRole,
     canListTicket,
     controller.list
   );
 
   router.get('/tickets/:id',
     authorizationMW.requiresAPILogin,
-    loadUserRole,
     canReadTicket,
     controller.get
   );
 
   router.post('/tickets/:id',
     authorizationMW.requiresAPILogin,
-    loadUserRole,
     canUpdateTicket,
     controller.update
   );
