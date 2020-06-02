d02d3e0d2fa4f039a394ffdb9233a1c7920abbeb
Khaled FERJANI <kferjani@linagora.com>
2020-06-02 10:19:50 +0200

    smartsla-frontend#625 additional default filters

--- backend/lib/filter/constants.js
+++ backend/lib/filter/constants.js
@@ -10,7 +10,17 @@ module.exports = {
     {
       _id: 'open',
       name: 'Open tickets',
-      query: { status: { $neq: 'closed' }}
+      query: { status: { $ne: 'closed' }}
+    },
+    {
+      _id: 'unassigned',
+      name: 'Unassigned tickets',
+      query: { assignedTo: { $exists: false }}
+    },
+    {
+      _id: 'suspended',
+      name: 'Suspended tickets',
+      query: { 'assignedTo.type': 'beneficiary' }
     }
   ]
 };
