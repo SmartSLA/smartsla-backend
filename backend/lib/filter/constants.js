2d1ba7ac11577885b235a53a05d788ee7606c7de
Khaled FERJANI <kferjani@linagora.com>
2020-06-03 14:35:49 +0100

    #625 fix ticket list order

--- backend/lib/filter/constants.js
+++ backend/lib/filter/constants.js
@@ -3,24 +3,25 @@
 module.exports = {
   FILTER_LIST: [
     {
-      _id: 'closed',
-      name: 'Closed tickets',
-      query: { status: 'closed' }
+      _id: 'mytickets',
+      name: 'My tickets',
+      query: { 'author.id': { $eq: '%user%' } }
     },
     {
-      _id: 'open',
-      name: 'Open tickets',
-      query: { status: { $ne: 'closed' }}
+      _id: 'myunsolvedtickets',
+      name: 'My unsolved tickets',
+      query: {
+        $or: [
+          { 'assignedTo._id': { $eq: '%user%' } },
+          { 'responsible._id': { $eq: '%user%' } }
+        ],
+        status: { $ne: 'closed' }
+      }
     },
     {
       _id: 'unassigned',
       name: 'Unassigned tickets',
-      query: { assignedTo: { $exists: false }}
-    },
-    {
-      _id: 'suspended',
-      name: 'Suspended tickets',
-      query: { 'assignedTo.type': 'beneficiary' }
+      query: { assignedTo: { $exists: false } }
     },
     {
       _id: 'recentlyupdated',
@@ -30,23 +31,23 @@ module.exports = {
     {
       _id: 'recentlysolved',
       name: 'Recently solved tickets',
-      query: { 'timestamps.updatedAt': { $gt: '%recent_date%' }, status: { $in: ['resolved', 'closed']} }
+      query: { 'timestamps.updatedAt': { $gt: '%recent_date%' }, status: { $in: ['resolved', 'closed'] } }
+    },
+
+    {
+      _id: 'open',
+      name: 'Open tickets',
+      query: { status: { $ne: 'closed' }}
     },
     {
-      _id: 'mytickets',
-      name: 'Your tickets',
-      query: { 'author.id': { $eq: '%user%'} }
+      _id: 'suspended',
+      name: 'Suspended tickets',
+      query: { 'assignedTo.type': 'beneficiary' }
     },
     {
-      _id: 'myunsolvedtickets',
-      name: 'Your unsolved tickets',
-      query: {
-        $or: [
-          { 'assignedTo._id': { $eq: '%user%' } },
-          { 'responsible._id': { $eq: '%user%' } }
-        ],
-        status: { $ne: 'closed'}
-      }
+      _id: 'closed',
+      name: 'Closed tickets',
+      query: { status: 'closed' }
     }
   ],
   RECENTLY: 86400000 // 24 hours
