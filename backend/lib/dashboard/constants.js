fd02dfafa6cf07d91fdee92deddc4c928d9c7923
Rachid OUBRAIM <roubraim@linagora.com>
2021-10-14 00:19:15 +0200

    smartsla-frontend#856 Add queries dashboard widget

--- backend/lib/dashboard/constants.js
+++ backend/lib/dashboard/constants.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { REQUEST_TYPE, TICKET_STATUS, SOFTWARE_CRITICAL, TICKETING_USER_TYPES } = require('../constants');
+const { REQUEST_TYPE, TICKET_STATUS, SOFTWARE_CRITICAL, TICKETING_USER_TYPES, SEVERITY_TYPE } = require('../constants');
 
 module.exports = function() {
 
@@ -198,6 +198,119 @@ module.exports = function() {
               _id: 0,
               softwareName: '$software.name',
               ticketCount: 1
+            }
+          }
+        ]
+      },
+      {
+        _id: 'ticketVolumeByType',
+        finalStages: [
+          {
+            $group: {
+              _id: null,
+              anomalyOpen: {
+                $sum: { $cond: { if: {$and: [{$eq: ['$type', REQUEST_TYPE.ANOMALY]}, { $ne: ['$status', TICKET_STATUS.CLOSED] }, {$ne: ['$archived', true] }] }, then: 1, else: 0 }}
+              },
+              anomalyClosed: {
+                $sum: { $cond: { if: {$and: [{$eq: ['$type', REQUEST_TYPE.ANOMALY]}, { $eq: ['$status', TICKET_STATUS.CLOSED] }, {$ne: ['$archived', true] }] }, then: 1, else: 0 }}
+              },
+              informationOpen: {
+                $sum: {$cond: { if: { $and: [{ $eq: ['$type', REQUEST_TYPE.INFORMATION] }, { $ne: ['$status', TICKET_STATUS.CLOSED] }, {$ne: ['$archived', true] }] }, then: 1, else: 0 }}
+              },
+              informationClosed: {
+                $sum: {$cond: { if: { $and: [{ $eq: ['$type', REQUEST_TYPE.INFORMATION] }, { $eq: ['$status', TICKET_STATUS.CLOSED] }, {$ne: ['$archived', true] }] }, then: 1, else: 0 }}
+              },
+              administrationOpen: {
+                $sum: {$cond: { if: { $and: [{ $eq: ['$type', REQUEST_TYPE.ADMINISTRATION] }, { $ne: ['$status', TICKET_STATUS.CLOSED] }, {$ne: ['$archived', true] }] }, then: 1, else: 0 }}
+              },
+              administrationClosed: {
+                $sum: {$cond: { if: { $and: [{ $eq: ['$type', REQUEST_TYPE.ADMINISTRATION] }, { $eq: ['$status', TICKET_STATUS.CLOSED] }, {$ne: ['$archived', true] }] }, then: 1, else: 0 }}
+              },
+              vulnerabilityOpen: {
+                $sum: {$cond: { if: { $and: [{ $eq: ['$type', REQUEST_TYPE.VULNERABILITY] }, { $ne: ['$status', TICKET_STATUS.CLOSED] }, {$ne: ['$archived', true] }] }, then: 1, else: 0 }}
+              },
+              vulnerabilityClosed: {
+                $sum: {$cond: { if: { $and: [{ $eq: ['$type', REQUEST_TYPE.VULNERABILITY] }, { $eq: ['$status', TICKET_STATUS.CLOSED] }, {$ne: ['$archived', true] }] }, then: 1, else: 0 }}
+              },
+              otherOpen: {
+                $sum: {$cond: { if: { $and: [otherType(), { $ne: ['$status', TICKET_STATUS.CLOSED] }, {$ne: ['$archived', true] }] }, then: 1, else: 0 }}
+              },
+              otherClosed: {
+                $sum: {$cond: { if: { $and: [otherType(), { $eq: ['$status', TICKET_STATUS.CLOSED] }, {$ne: ['$archived', true] }] }, then: 1, else: 0 }}
+              }
+            }
+          },
+          {
+            $project: {
+              _id: 0,
+              anomaly: {
+                total: {$sum: ['$anomalyOpen', '$anomalyClosed']},
+                open: '$anomalyOpen',
+                closed: '$anomalyClosed'
+              },
+              information: {
+                total: {$sum: ['$informationOpen', '$informationClosed']},
+                open: '$informationOpen',
+                closed: '$informationClosed'
+              },
+              administration: {
+                total: {$sum: ['$administrationOpen', '$administrationClosed']},
+                open: '$administrationOpen',
+                closed: '$administrationClosed'
+              },
+              vulnerability: {
+                total: {$sum: ['$vulnerabilityOpen', '$vulnerabilityClosed']},
+                open: '$vulnerabilityOpen',
+                closed: '$vulnerabilityClosed'
+              },
+              other: {
+                total: {$sum: ['$otherOpen', '$otherClosed']},
+                open: '$otherOpen',
+                closed: '$otherClosed'
+              }
+            }
+          }
+        ]
+      },
+      {
+        _id: 'ticketPriorityAnomalies',
+        finalStages: [
+          {
+            $match: {
+              type: REQUEST_TYPE.ANOMALY,
+              archived: { $ne: true }
+            }
+          },
+          {
+            $group: {
+              _id: null,
+              blockingCritical: {
+                $sum: { $cond: { if: {$and: [{$eq: ['$severity', SEVERITY_TYPE.BLOCKING]}, { $eq: ['$software.critical', SOFTWARE_CRITICAL.CRITICAL] }] }, then: 1, else: 0 }}
+              },
+              blockingNotCritical: {
+                $sum: { $cond: { if: {$and: [{$eq: ['$severity', SEVERITY_TYPE.BLOCKING]}, { $ne: ['$software.critical', SOFTWARE_CRITICAL.CRITICAL] }] }, then: 1, else: 0 }}
+              },
+              noBlockingCritical: {
+                $sum: {$cond: { if: { $and: [{ $ne: ['$severity', SEVERITY_TYPE.BLOCKING] }, { $eq: ['$software.critical', SOFTWARE_CRITICAL.CRITICAL] }] }, then: 1, else: 0 }}
+              },
+              noBlockingNotCritical: {
+                $sum: {$cond: { if: { $and: [{ $ne: ['$severity', SEVERITY_TYPE.BLOCKING] }, { $ne: ['$software.critical', SOFTWARE_CRITICAL.CRITICAL] }] }, then: 1, else: 0 }}
+              }
+            }
+          },
+          {
+            $project: {
+              _id: 0,
+              blocking: {
+                total: {$sum: ['$blockingCritical', '$blockingNotCritical']},
+                blockingCritical: '$blockingCritical',
+                blockingNotCritical: '$blockingNotCritical'
+              },
+              noBlocking: {
+                total: {$sum: ['$noBlockingCritical', '$noBlockingNotCritical']},
+                blockingCritical: '$noBlockingCritical',
+                blockingNotCritical: '$noBlockingNotCritical'
+              }
             }
           }
         ]
